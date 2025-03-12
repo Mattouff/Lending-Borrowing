3143a07f810b863239754fe9f64612b908c5ef4a
Mattouff <92375190+Mattouff@users.noreply.github.com>
2025-03-12 14:13:36 +0100

    Feat/workflow (#5)
    * add workflow
    
    * update workflow
    
    * moove the gitmodules and update the workflow
    
    * formattage by foundry
    
    * change the solidity version
    
    * change the foundry.toml

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.29;
 
 import "forge-std/Script.sol";
 import "../src/Token.sol";
@@ -8,16 +8,16 @@ import "../src/LendingPool.sol";
 contract DeployToken is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        
+
         vm.startBroadcast(deployerPrivateKey);
-        
+
         uint256 initialSupply = 1000 * 10 ** 18;
-        
+
         Token token = new Token(initialSupply);
         LendingPool lendingPool = new LendingPool(address(token));
-        
+
         vm.stopBroadcast();
-        
+
         console.log("Token deployed :", address(token));
         console.log("LendingPool deployed at:", address(lendingPool));
     }
